df26bcf00ff29712ca00db7602d5e2f79fa6fb5c
Jin Baek Hong <65929678+hjjam100@users.noreply.github.com>
2022-06-02 00:27:51 +0900

    feat: Login (#1)
    * feat: zustand state setting
    
    * chore: init env
    
    * feat: set axiosUtil & storageUtil
    
    * feat: set login service with react-query
    
    * feat: Login & LoginForm view
    
    * refactor: types
    
    * refactor: set type to storage parsed value

--- src/features/auth/services/index.ts
+++ src/features/auth/services/index.ts
@@ -1,4 +1,41 @@
-// import axios from 'axios'
-// import { useQuery } from 'react-query'
+import { AxiosResponse } from 'axios'
+import { useQuery } from 'react-query'
+import Axios from '../../../utils/axiosUtil'
+import { Authentication, LoginPayload, LoginResponse } from '../types'
 
-export const authService = {}
+export const authService = {
+  async login(payload: LoginPayload) {
+    const res: AxiosResponse<LoginResponse> = await Axios.post('/auth/login', {
+      accountId: payload.accountId,
+      password: payload.password,
+      authentication: Authentication.STATICKS,
+    })
+
+    return res.data
+  },
+}
+
+function useAuthService(payload: LoginPayload | any) {
+  const { data, error, isLoading, refetch } = useQuery(
+    [
+      'auth',
+      'login',
+      `accountId=${payload.accountId}`,
+      `password=${payload.password}`,
+    ],
+    () => authService.login(payload),
+    {
+      enabled: !!payload.accountId && !!payload.password,
+      staleTime: Infinity,
+      retry: false,
+    },
+  )
+  return {
+    data,
+    error,
+    isLoading,
+    refetch,
+  }
+}
+
+export default useAuthService
---
+++ src/utils/storageUtil.ts
@@ -0,0 +1,14 @@
+import { IStore } from '../features/store'
+
+interface IstorageStore {
+  store: IStore
+}
+
+export const getToken = (): string | null => {
+  const storageData = localStorage.getItem('staticks-store')
+
+  if (!storageData) return null
+
+  const parsedData: IstorageStore = JSON.parse(storageData)
+  return parsedData?.store?.token
+}
